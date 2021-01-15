613ea74452622d699dd72f6b09ffce1029bae78f
Your name here <your_email_address@example.com>
2021-01-15 15:53:04 +0200

    add fix js

--- fix.js
+++ fix.js
@@ -1,1 +1,24 @@
 console.log('Loaded');
+
+const RunCarouselFix = function () {
+    // owl-nav
+    const sections = document.querySelectorAll('.sect-carou');
+    sections.forEach(section => {
+        const nav = section.querySelector('.owl-nav');
+        const imgs = [...section.querySelectorAll('img')].filter(img => img.hasAttribute('data-src'));
+        nav.addEventListener('click', () => {
+            imgs.some((el, i, array) => {
+                if (el.src === el.getAttribute('data-src')) {
+                    const first = array[i + 1];
+                    const second = array[i + 2];
+                    first && setSrc(first);
+                    second && setSrc(second);
+                    return el;
+                }
+            })
+        });
+    });
+    const setSrc = function (el) {
+        el.src = el.getAttribute('data-src');
+    }
+}
